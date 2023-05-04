f586bb8cdb6aa7cdb6d3f8cd02c18a4b6b0ad9ef
TwitchC4T <74559314+BoxC4T@users.noreply.github.com>
2023-05-04 00:39:48 -0500

    Update pwnp.c

--- pwnp/pwnp.c
+++ pwnp/pwnp.c
@@ -14,7 +14,11 @@ static bool init_pico();
 static bool init_battery();
 static bool init_screen();
 static void main_loop();
+static void init_image();
 UBYTE *BlackImage;
+int battery_percent = 100;
+
+
 
 int main() {
     if (init_pico() == true) {
@@ -54,25 +58,42 @@ bool init_screen(){
         printf("Failed to apply for black memory...\r\n");
         return false;
     }
-    
-    Paint_NewImage(BlackImage, EPD_2in13_V3_WIDTH, EPD_2in13_V3_HEIGHT, 270, WHITE);  	
-    Paint_SelectImage(BlackImage);
-    Paint_Clear(WHITE);
-    Paint_DrawString_EN(140, 15, "FUCK YOU", &Font12, BLACK, WHITE);
-    EPD_2in13_V3_Display_Base(BlackImage);
-    sleep_ms(250);
-    Paint_DrawString_EN(140, 30, "FUCK YOU", &Font16, WHITE, BLACK);
-    EPD_2in13_V3_Display_Base(BlackImage);
     cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
     sleep_ms(250);
     cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
     sleep_ms(250);
     return true;
 }
+void update_battery_display(){
+    int length = snprintf( NULL, 0, "%d", battery_percent );
+    char* num = malloc( length + 1 );
+    snprintf( num, length + 1, "%d", battery_percent );
+    char* str = malloc( length + 2 );
+    char* prt = "%";
+    strcpy(str, num);
+    strcat(str, prt);
+    free(num);
+    Paint_DrawString_EN(5, 2, str, &Font12, WHITE, BLACK);
+    free(str);
+    // TODO
+}
+
+
+
 void main_loop(){
-    Paint_DrawString_EN(140, 15, "Testing", &Font12, BLACK, WHITE);
+    init_image();
     EPD_2in13_V3_Display_Base(BlackImage);
-    sleep_ms(250);
-    Paint_DrawString_EN(140, 30, "Testing", &Font16, WHITE, BLACK);
-    EPD_2in13_V3_Display_Base(BlackImage);
+    
+}
+
+void init_image(){
+    Paint_NewImage(BlackImage, EPD_2in13_V3_WIDTH, EPD_2in13_V3_HEIGHT, 270, WHITE);  	
+    Paint_SelectImage(BlackImage);
+    Paint_Clear(WHITE);
+    Paint_DrawString_EN(180, 5, "HOSTNAME", &Font12, WHITE, BLACK);
+    Paint_DrawLine(0,15,250,15, BLACK, DOT_PIXEL_1X1, LINE_STYLE_SOLID);
+    Paint_DrawLine(0,107,250,107, BLACK, DOT_PIXEL_1X1, LINE_STYLE_SOLID);
+    Paint_DrawString_EN(20, 50, "(OwO)", &Font24, WHITE, BLACK);
+    update_battery_display();
+
 }
